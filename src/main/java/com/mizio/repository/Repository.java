4ff1adabe9b912a9f915fcf247dd794b0b9f7f53
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-05 20:35:17 +0200

    add interface methods (CRUD) for Test object

--- src/main/java/com/mizio/repository/Repository.java
+++ src/main/java/com/mizio/repository/Repository.java
@@ -1,11 +1,16 @@
 package com.mizio.repository;
 
 import com.mizio.model.Subject;
+import com.mizio.model.Test;
 
 public interface Repository {
 
     void saveOrUpdateSubject(Subject subject);
     Subject getSubject(int subjectID);
     boolean deleteSubject(int subjectID);
+
+    void saveOrUpdateTest(int subjectID, Test test);
+    Test getTest(int testID);
+    boolean deleteTest(int testID);
 
 }
