6a5d362f880b8e84fc39aed110431d41740f627d
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-06 21:50:38 +0200

    remove CRUD methods for Subject, Test and Question objects

--- src/main/java/com/mizio/repository/Repository.java
+++ src/main/java/com/mizio/repository/Repository.java
@@ -1,24 +1,8 @@
 package com.mizio.repository;
 
-import com.mizio.model.Question;
-import com.mizio.model.Subject;
-import com.mizio.model.Test;
-
 public interface Repository {
 
     <T> void saveOrUpdateObject(T object);
     <T> T getObject(Class<T> tClass, int ID);
     <T> boolean deleteObject(Class<T> tClass, int ID);
-
-    void saveOrUpdateSubject(Subject subject);
-    Subject getSubject(int subjectID);
-    boolean deleteSubject(int subjectID);
-
-    void saveOrUpdateTest(Test test);
-    Test getTest(int testID);
-    boolean deleteTest(int testID);
-
-    void saveOrUpdateQuestion(Question question);
-    Question getQuestion(int questionID);
-    boolean deleteQuestion(int questionID);
 }
--- src/main/java/com/mizio/repository/RepositoryImpl.java
+++ src/main/java/com/mizio/repository/RepositoryImpl.java
@@ -1,8 +1,5 @@
 package com.mizio.repository;
 
-import com.mizio.model.Question;
-import com.mizio.model.Subject;
-import com.mizio.model.Test;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -50,92 +47,6 @@ public class RepositoryImpl implements Repository {
             return true;
         }
         closeSessionAndSessionFactory();
-        return false;
-    }
-
-    @Override
-    public void saveOrUpdateSubject(Subject subject) {
-        configureSessionAndBeginTransaction();
-        session.saveOrUpdate(subject);
-        session.getTransaction().commit();
-        closeSessionAndSessionFactory();
-    }
-
-    @Override
-    public Subject getSubject(int subjectID) {
-        configureSessionAndBeginTransaction();
-        Subject subject = session.get(Subject.class, subjectID);
-        closeSessionAndSessionFactory();
-        return subject;
-    }
-
-    @Override
-    public boolean deleteSubject(int subjectID) {
-        configureSessionAndBeginTransaction();
-        Subject subject = session.load(Subject.class, subjectID);
-        if(subject != null) {
-            session.delete(subject);
-            session.getTransaction().commit();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void saveOrUpdateTest(Test test) {
-        configureSessionAndBeginTransaction();
-        session.saveOrUpdate(test);
-        session.getTransaction().commit();
-        closeSessionAndSessionFactory();
-    }
-
-    @Override
-    public Test getTest(int testID) {
-        configureSessionAndBeginTransaction();
-        Test test = session.get(Test.class, testID);
-        closeSessionAndSessionFactory();
-        return test;
-    }
-
-    @Override
-    public boolean deleteTest(int testID) {
-        configureSessionAndBeginTransaction();
-        Test test = session.find(Test.class, testID);
-        if(test != null) {
-            session.delete(test);
-            session.getTransaction().commit();
-            closeSessionAndSessionFactory();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void saveOrUpdateQuestion(Question question) {
-        configureSessionAndBeginTransaction();
-        session.saveOrUpdate(question);
-        session.getTransaction().commit();
-        closeSessionAndSessionFactory();
-    }
-
-    @Override
-    public Question getQuestion(int questionID) {
-        configureSessionAndBeginTransaction();
-        Question question = session.get(Question.class, questionID);
-        closeSessionAndSessionFactory();
-        return question;
-    }
-
-    @Override
-    public boolean deleteQuestion(int questionID) {
-        configureSessionAndBeginTransaction();
-        Question question = session.find(Question.class, questionID);
-        if(question != null) {
-            session.delete(question);
-            session.getTransaction().commit();
-            closeSessionAndSessionFactory();
-            return true;
-        }
         return false;
     }
 }
