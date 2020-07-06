c140c508a8f00543302baef7ecb68e178969bfa3
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-06 21:44:59 +0200

    add interface generic methods (CRUD) for entities

--- src/main/java/com/mizio/repository/Repository.java
+++ src/main/java/com/mizio/repository/Repository.java
@@ -1,21 +1,24 @@
 package com.mizio.repository;
 
-import com.mizio.model.AnswersContent;
 import com.mizio.model.Question;
 import com.mizio.model.Subject;
 import com.mizio.model.Test;
 
 public interface Repository {
 
+    <T> void saveOrUpdateObject(T object);
+    <T> T getObject(Class<T> tClass, int ID);
+    <T> boolean deleteObject(Class<T> tClass, int ID);
+
     void saveOrUpdateSubject(Subject subject);
     Subject getSubject(int subjectID);
     boolean deleteSubject(int subjectID);
 
-    void saveOrUpdateTest(int subjectID, Test test);
+    void saveOrUpdateTest(Test test);
     Test getTest(int testID);
     boolean deleteTest(int testID);
 
-    void saveOrUpdateQuestion(Question question, AnswersContent answersContent);
+    void saveOrUpdateQuestion(Question question);
     Question getQuestion(int questionID);
     boolean deleteQuestion(int questionID);
 }
