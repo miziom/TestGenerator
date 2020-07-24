37569e0f7601e9331fdaf07348338ebb1445d09f
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-24 16:02:57 +0200

    add get and check Test objects methods for Subject object

--- src/main/java/com/mizio/repository/RepositoryListViewer.java
+++ src/main/java/com/mizio/repository/RepositoryListViewer.java
@@ -1,6 +1,7 @@
 package com.mizio.repository;
 
 import com.mizio.model.Subject;
+import com.mizio.model.Test;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,14 +18,49 @@ public class RepositoryListViewer {
     }
 
     public Subject getSubject(int ID) {
-        return subjectList.stream()
-                .filter(s -> s.getSubjectID() == ID).findFirst().get();
+        Optional<Subject> subjectOptional = subjectList.stream()
+                .filter(s -> s.getSubjectID() == ID)
+                .findFirst();
+        return subjectOptional.orElse(null);
     }
 
     public boolean subjectIsPresent(String subjectName) {
         Optional<Subject> subject = subjectList.stream()
-                .filter(s -> s.getSubjectName().equals(subjectName)).findFirst();
+                .filter(s -> s.getSubjectName().equals(subjectName))
+                .findFirst();
         return subject.isPresent();
+    }
+
+    public Test getTest(int subjectID, int testID) {
+        Optional<Subject> subjectOptional = subjectList.stream()
+                .filter(subject -> subject.getSubjectID() == subjectID)
+                .findFirst();
+        if (subjectOptional.isPresent()) {
+            Optional<Test> testOptional = subjectOptional.get()
+                    .getTests()
+                    .stream()
+                    .filter(test -> test.getTestID() == testID)
+                    .findFirst();
+            return testOptional.orElse(null);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean testIsPresent(int subjectID, String testName) {
+        Optional<Subject> subjectOptional = subjectList.stream()
+                .filter(subject -> subject.getSubjectID() == subjectID)
+                .findFirst();
+        if (subjectOptional.isPresent()) {
+            Optional<Test> testOptional = subjectOptional.get()
+                    .getTests()
+                    .stream()
+                    .filter(test -> test.getTestName().equals(testName))
+                    .findFirst();
+            return testOptional.isPresent();
+        } else {
+            return false;
+        }
     }
 
 }
