db3a3bcbf1bf9e38b899f33d30ec4ed061d61294
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-26 19:52:38 +0200

    add title pattern used in pupUpManager

--- src/main/java/com/mizio/pattern/TitlePattern.java
+++ src/main/java/com/mizio/pattern/TitlePattern.java
@@ -8,4 +8,8 @@ public class TitlePattern {
     public static final String ADD_FILE_VIEW        = "Generator Testów - Dodaj Plik";
     public static final String GENERATE_VIEW        = "Generator Testów - Generuj Test";
     public static final String SETTINGS_VIEW        = "Generator Testów - Ustawienia";
+    public static final String YES                  = "TAK";
+    public static final String NO                   = "NIE";
+    public static final String DELETE_PUP_UP        = "Usuwanie obiektów";
+    public static final String DELETE_AMOUNT_PUP_UP = "Czy chcesz usunąć %d obiektów";
 }
