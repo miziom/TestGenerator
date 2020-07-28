97fc2a29574fde7e796948b05fe4a524df4662e2
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-07-28 11:33:53 +0200

    add table view for added test for chosen subject and allow add new test object by user

--- src/main/java/com/mizio/controller/AddTestController.java
+++ src/main/java/com/mizio/controller/AddTestController.java
@@ -4,13 +4,19 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import com.mizio.manager.ViewManager;
+import com.mizio.model.Subject;
+import com.mizio.model.Test;
 import com.mizio.pattern.PathPattern;
 import com.mizio.pattern.TitlePattern;
+import com.mizio.repository.RepositoryListViewer;
+import com.mizio.repository.RepositoryService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
@@ -18,17 +24,21 @@ import java.util.ResourceBundle;
 
 public class AddTestController implements Initializable {
 
+    private RepositoryService repositoryService = new RepositoryService();
+
+    private RepositoryListViewer repositoryListViewer = new RepositoryListViewer();
+
     @FXML
     private JFXButton buttonBack;
 
     @FXML
-    private JFXComboBox<?> comboBoxSubject;
+    private JFXComboBox<Subject> comboBoxSubject;
 
     @FXML
-    private TableView<?> tableView;
+    private TableView<Test> tableView;
 
     @FXML
-    private TableColumn<?, ?> columnTestName;
+    private TableColumn<Test, String> columnTestName;
 
     @FXML
     private JFXTextField textFieldTestName;
@@ -38,7 +48,8 @@ public class AddTestController implements Initializable {
 
     @FXML
     void buttonAddTestAction(ActionEvent event) {
-
+        addTest();
+        tableViewRefresh();
     }
 
     @FXML
@@ -48,16 +59,57 @@ public class AddTestController implements Initializable {
 
     @FXML
     void comboBoxSubjectAction(ActionEvent event) {
-
+        tableViewRefresh();
     }
 
     @FXML
     void isLetterAction(KeyEvent event) {
+        checkButton();
+    }
 
+    @FXML
+    void textFieldTestNameAction(ActionEvent event) {
+        addTest();
+        tableViewRefresh();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttonAddTest.setDisable(true);
+        comboBoxSubject.getItems().setAll(repositoryListViewer.getSubjectList());
+        if (!repositoryListViewer.getSubjectList().isEmpty()) {
+            comboBoxSubject.getSelectionModel().selectFirst();
+            tableViewRefresh();
+        }
+    }
 
+    private void checkButton() {
+        if (textFieldTestName.getText().isBlank()) {
+            buttonAddTest.setDisable(true);
+        } else {
+            buttonAddTest.setDisable(false);
+        }
+    }
+
+    private void tableViewRefresh() {
+        tableView.getItems().clear();
+        tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        columnTestName.setCellValueFactory(new PropertyValueFactory<>("testName"));
+        tableView.getItems().setAll(repositoryListViewer.getSubject(
+                comboBoxSubject.getSelectionModel()
+                        .getSelectedItem()
+                        .getSubjectID())
+                .getTests());
+    }
+
+    private void addTest() {
+        Test test = Test.builder()
+                .subject(comboBoxSubject.getSelectionModel().getSelectedItem())
+                .testName(textFieldTestName.getText().trim())
+                .build();
+        repositoryService.saveOrUpdateObject(test);
+        repositoryListViewer.saveOrUpdateList();
+        textFieldTestName.clear();
+        buttonAddTest.setDisable(true);
     }
 }
