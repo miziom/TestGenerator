3a58a4ccdd2a21ac627c76abee7b660c04f6ba91
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-08-25 16:39:18 +0200

    add string paterns for labels in editQuestionView

---
+++ src/main/java/com/mizio/pattern/LabelPattern.java
@@ -0,0 +1,9 @@
+package com.mizio.pattern;
+
+public class LabelPattern {
+    public static final String QUESTION_OLD_NAME    = "Poprzednie pytanie: %s";
+    public static final String QUESTION_OLD_A       = "Poprzednia odp. A: %s";
+    public static final String QUESTION_OLD_B       = "Poprzednia odp. B: %s";
+    public static final String QUESTION_OLD_C       = "Poprzednia odp. C: %s";
+    public static final String QUESTION_OLD_D       = "Poprzednia odp. D: %s";
+}
