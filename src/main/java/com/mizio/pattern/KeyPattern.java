d7ec33131d49482fb9565cbc0f35bcd4c9759f36
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-09-04 00:06:14 +0200

    allow to create excel key file

---
+++ src/main/java/com/mizio/pattern/KeyPattern.java
@@ -0,0 +1,9 @@
+package com.mizio.pattern;
+
+public class KeyPattern {
+
+    public static final String TITLE_SHEET_KEY       = "Klucz";
+    public static final String TITLE_SURNAME         = "NAZWISKO";
+    public static final String TITLE_NAME            = "IMIĘ";
+    public static final String TITLE_GROUP           = "GR.";
+}
